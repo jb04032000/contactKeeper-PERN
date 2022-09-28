3dc3ce0b8343177311bbeaf7a274cd0cf662f7df
jb0403 <jayeshb7548@gmail.com>
2022-09-28 18:15:20 +0530

    - setup react app for frontend development - setup eslint, prettier, sass, react bootstrap & parcel

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import Button from "react-bootstrap/Button";
+import "./styles/App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Button variant="primary">Primary</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
